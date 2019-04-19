30ae19e4f1c964afdaa056f122a6d7f3ca17cdc1
Daniel Domurad <daniel.domurad@makingwaves.com>
2019-04-19 08:49:46 +0200

    Unused using removed

--- EPiServer.Vsf.ApiBridge/Controllers/UserController.cs
+++ EPiServer.Vsf.ApiBridge/Controllers/UserController.cs
@@ -2,7 +2,6 @@
 using System.Web.Http;
 using EPiServer.Vsf.ApiBridge.Authorization;
 using EPiServer.Vsf.Core.ApiBridge.Endpoint;
-using EPiServer.Vsf.Core.ApiBridge.Model;
 using EPiServer.Vsf.Core.ApiBridge.Model.Authorization;
 using EPiServer.Vsf.Core.ApiBridge.Model.User;
 using Microsoft.AspNet.Identity;
